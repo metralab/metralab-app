93db108090b855bd6093b2ad2db6bf1bd1b15d71
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2021-01-10 17:59:34 +0100

    Highlight item summary if tapped

--- lib/components/item_summary.dart
+++ lib/components/item_summary.dart
@@ -6,13 +6,20 @@ import '../data/items_provider.dart';
 import '../data/item.dart';
 
 class ItemSummary extends StatelessWidget {
-  const ItemSummary(this.item, {Key key, this.onTapped}) : super(key: key);
+  const ItemSummary(
+    this.item, {
+    Key key,
+    @required this.onTapped,
+    this.selected = false,
+  }) : super(key: key);
 
   final Item item;
+  final bool selected;
   final void Function(Item) onTapped;
 
   @override
   Widget build(BuildContext context) => Card(
+        color: selected ? Theme.of(context).selectedRowColor : null,
         child: ListTile(
           title: Text(tr('itemSummary', args: [item.birth.toString()])),
           trailing: IconButton(
